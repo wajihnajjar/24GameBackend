7eb5f69dac00bf5953a0768bfe180cb68a36efb8
wajihnajjar <wajih.najjar@esprit.tn>
2023-09-18 02:44:23 +0100

    Creating Function That Create all Combination

---
+++ controllers/createLevel.controllers.js
@@ -0,0 +1,25 @@
+const db= require("./database/index")
+
+const CreateAllArrays = ()=>{
+    let  arr = []
+    let tempArray = [-1 , -1  , -1 , -1]
+    for(let i = 0 ; i<10 ; i ++){
+        tempArray[0] = i
+        for (let j = 0  ;j< 10 ; j ++){
+            tempArray[1] = j
+            for (let k = 0 ; k< 10 ; k ++){
+                tempArray[2] = k 
+                for (r = 0 ; r < 10 ; r++){
+                    tempArray[3] = r 
+                    arr.push(tempArray)
+                }
+            }
+        }
+    }
+    console.log(arr)
+
+}
+module.exports={
+    CreateAllArrays
+
+}
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express  =require("express")
 const server = require("express")()
 const cors = require("cors");
-
 server.use(cors({ origin: "*" })); 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
