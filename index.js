30d3ba29d586b3cf98e003858624c2f7c9cd0d90
wajihnajjar <wajih.najjar@esprit.tn>
2023-09-18 02:29:36 +0100

    Installing Middleware and using it

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express  =require("express")
 const server = require("express")()
+const cors = require("cors");
+
+server.use(cors({ origin: "*" })); 
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.get("/",(req,res)=>{
+    res.send("<h1>Hello World </h1>")
+})
+
 server.listen(4001 ,()=>{
     console.log("Connected in 4001")
 })
