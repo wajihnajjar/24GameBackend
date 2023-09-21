59b56165973a35669e04f0e39eca1cdb4f5ed119
wajihnajjar <wajih.najjar@esprit.tn>
2023-09-21 03:22:40 +0100

    Fixing Bugs.

--- controllers/createLevel.controllers.js
+++ controllers/createLevel.controllers.js
@@ -33,15 +33,14 @@ for (let i = 0 ; i< arr.length ; i ++){
     for (let j = 0  ;j< arr.length; j ++){
         if(i==j)
             continue ; 
+       
         let x = arr[i] 
         let y = arr[j]  
+        console.log(arr,  " ", x ," " ,  y )
         arr.splice(i,1)
-        arr.splice(j,1)
         arr.push(x+y)
-        if(arr.length == 2 )
-        console.log(arr," ",  i , " " ,  j )
+        arr.splice(j,1)
         helper(arr,index,sum)
-       return 
     }
 } 
 
