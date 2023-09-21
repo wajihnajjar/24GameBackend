25a3ec9827254efe7ce3710a088e9c338840fde5
wajihnajjar <wajih.najjar@esprit.tn>
2023-09-21 03:09:45 +0100

    Fixing Bugs...

--- controllers/createLevel.controllers.js
+++ controllers/createLevel.controllers.js
@@ -25,6 +25,8 @@ const CreateAllArrays =async  (req,res)=>{
 //  1 2 3 4 
 // 
 const helper = (arr,  index,sum)=>{
+    if(arr.length == 3 )
+    console.log(arr)
 if(arr.length==1 ){
     console.log(arr  ," ", sum)
 return 
