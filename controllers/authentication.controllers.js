95b8fc07d531f67f8e0368d66a799fafd92831ae
wajihnajjar <wajih.najjar@esprit.tn>
2023-10-12 17:10:34 +0100

    Adding Authentication

--- controllers/authentication.controllers.js
+++ controllers/authentication.controllers.js
@@ -6,17 +6,20 @@ exports.login = (req,res)=>{
 res.send("Logged")
 }
 exports.signUp = async  (req,res)=>{
-    const {email , password,name } = req.body 
+    const {email , password,name , token0 } = req.body 
      const hashedPass = await bcrypt.hash(password , 10) 
      const token = jwt.sign(
         { user_id:0, email },
         process.env.JWT_Key,
         {
-          expiresIn: "5h",
+          expiresIn: "5m",
         }
       );
-       
-      res.send.json({
+      console.log(token)
+      const decoded = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjowLCJlbWFpbCI6IkAiLCJpYXQiOjE2OTcxMjY5ODgsImV4cCI6MTY5NzEyNzI4OH0.s9muZxdp5ucaqgy-dzc9UT6mzdxIKOBruUJ9Micf2ws", process.env.JWT_Key)
+      console.log(decoded)
+
+      res.status(200).json({
         message: "Autheticated", 
         token:token
       })
