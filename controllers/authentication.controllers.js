49f93d2d43e2b00628079da9ba0f2757b7556415
wajihnajjar <wajih.najjar@esprit.tn>
2023-10-12 16:57:15 +0100

    Adding Authentication

--- controllers/authentication.controllers.js
+++ controllers/authentication.controllers.js
@@ -1,4 +1,9 @@
+const bcrypt = require("bcrypt")
 exports.login = (req,res)=>{
- res.send("Looged")   
-
+res.send("Logged")
+}
+exports.signUp = async  (req,res)=>{
+    const {email , password,name } = req.body 
+     const hashedPass = await bcrypt.hash(password , 10) 
+     
 }
