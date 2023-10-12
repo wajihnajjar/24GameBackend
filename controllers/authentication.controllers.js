052d26095e732e894000b1dd24f88eec648cdc90
wajihnajjar <wajih.najjar@esprit.tn>
2023-10-12 16:59:51 +0100

    Adding Authentication

--- controllers/authentication.controllers.js
+++ controllers/authentication.controllers.js
@@ -1,9 +1,19 @@
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 exports.login = (req,res)=>{
 res.send("Logged")
 }
 exports.signUp = async  (req,res)=>{
     const {email , password,name } = req.body 
      const hashedPass = await bcrypt.hash(password , 10) 
-     
+     const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+       let a =token 
+      res.send(a)
+        
 }
--- routes/authentication.routes.js
+++ routes/authentication.routes.js
@@ -1,4 +1,5 @@
 const authentication  = require("../controllers/authentication.controllers")
 const router = require('express').Router()
 router.post("/login",authentication.login)
+router.post("/signup" , authentication.signUp)
 module.exports = router
