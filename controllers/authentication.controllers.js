89b92cce718eef70c970e3004def5f6e4db51cf2
wajihnajjar <wajih.najjar@esprit.tn>
2023-10-15 00:18:12 +0100

    ..

--- controllers/authentication.controllers.js
+++ controllers/authentication.controllers.js
@@ -9,16 +9,13 @@ exports.signUp = async  (req,res)=>{
     const {email , password,name , token0 } = req.body 
      const hashedPass = await bcrypt.hash(password , 10) 
      const token = jwt.sign(
-        { user_id:0, email },
+        { password:hashedPass, email },
         process.env.JWT_Key,
         {
-          expiresIn: "5m",
+          expiresIn: "2h",
         }
       );
       console.log(token)
-      //const decoded = jwt.verify(token, process.env.JWT_Key)
-      //console.log(decoded)
-
       res.status(200).json({
         message: "Autheticated", 
         token:token
